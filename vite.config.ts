d3327d0ea125a09c358c7f92c4b18f50c1007249
mrmateussiilva <mateuspython3010@gmail.com>
2026-01-19 11:51:07 -0300

    fix: resolve buffer alias for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
@@ -16,7 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      buffer: 'buffer/',
+      buffer: require.resolve('buffer/'),
     },
   },
   optimizeDeps: {
